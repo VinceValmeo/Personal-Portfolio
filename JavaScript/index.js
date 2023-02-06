ca55fe3cce59f45fe6e9921be35453433bf8f002
Vnc <vivalmeo@gmail.com>
2023-02-06 13:54:53 +0800

    Typing animation

---
+++ JavaScript/index.js
@@ -0,0 +1,8 @@
+var typingEffect = new Typed(".multi-text", {
+    strings: ["Software Developer", "Web App Developer", "Graphic Designer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    startDelay: 1000,
+    backDelay: 1500
+});
